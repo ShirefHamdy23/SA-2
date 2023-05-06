d2571c53f881828c922cf4ca4772c71fc1b694d7
adham-gaber <eng.adham.gaber@gmail.com>
2023-05-07 00:25:36 +0300

    connect error

--- Client/controllers/kafka_consumer_controller.js
+++ Client/controllers/kafka_consumer_controller.js
@@ -6,7 +6,7 @@ let consumer;
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
-  await prisma.connect()
+  await prisma.$connect()
   console.log(await prisma.users.count())
 }
 main().catch(console.error)
